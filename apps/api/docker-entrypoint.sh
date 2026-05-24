c7c57204efa789c588ec35f192005cda5987e503
Abraham Vilches <123977875+AbrahamVilchesDeLaCruz@users.noreply.github.com>
2026-05-24 19:50:22 +0200

    fix(infra): use typeorm/cli.js directly instead of .bin/typeorm wrapper (#29)

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running migrations..."
-node node_modules/.bin/typeorm migration:run -d dist/src/shared/infrastructure/persistence/typeorm/typeorm.config.cli.js
+node node_modules/typeorm/cli.js migration:run -d dist/src/shared/infrastructure/persistence/typeorm/typeorm.config.cli.js
 
 echo "Starting app..."
 exec node dist/src/main
